97ec5ea700a6b814d56478fc9a590b2c6877435d
chiarasanasi <chiarasanasi.work@gmail.com>
2025-05-05 14:47:39 +0200

    first commit

---
+++ src/Esercizio1.java
@@ -0,0 +1,18 @@
+public class Esercizio1 {
+    public static void main(String[] args) {
+        System.out.println(moltiplica(5,6));
+        String risultatoConcatena = concatena("Chiara", 26);
+        System.out.println(risultatoConcatena);
+        String[] parole = {"Chiara","Checco","Rosa","Eli","Angela","Giuseppe"};
+    }
+    public static int moltiplica (int a, int b){
+        return  a*b;
+    }
+    public static String concatena(String c, int d){
+        return c+d;
+    }
+
+    public static String inserisciInArray(String[] e[5],int f){
+        return e[]
+    }
+}
